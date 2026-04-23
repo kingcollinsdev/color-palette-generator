78fb0a6bfbf5dfac77f7f115e03bb75fd0a7af61
CollinsWachira95 <Collins.Wachira@student.moringaschool.com>
2026-04-23 11:12:25 +0200

    Fixed the copy button

--- script.js
+++ script.js
@@ -4,31 +4,31 @@ const paletteContainer = document.querySelector(".palette-container");
 generateBtn.addEventListener("click", generatePalette);
 
 paletteContainer.addEventListener("click", function (e) {
-    if (e.target.classlist.contains("copy-btn")) {
+    if(e.target.classList.contains("copy-btn")) {
         const hexValue = e.target.previousElementSibling.textContent;
 
         navigator.clipboard
         .writeText(hexValue)
         .then(() => showCopySuccess(e.target))
         .catch((err) => console.log(err));
-    } else if (e.target.classlist.contains("color")) {
+    } else if (e.target.classList.contains("color")) {
         const hexValue = e.target.nextElementSibling.querySelector(".hex-value").textContent;
         navigator.clipboard
         .writeText(hexValue)
         .then(() => showCopySuccess(e.target.nextElementSibling.querySelector(".copy-btn")))
         .catch((err) => console.log(err));
     }
-});
+})
 
 function showCopySuccess(element) {
-    element.classlist.remove("far", "fa-copy");
-    element.classlist.add("fas", "fa-check");
+    element.classList.remove("far", "fa-copy");
+    element.classList.add("fas", "fa-check");
 
     element.style.color = "#48bb78";
 
     setTimeout(() => {
-    element.classlist.remove("fas", "fa-check");
-    element.classlist.add("far", "fa-copy");
+    element.classList.remove("fas", "fa-check");
+    element.classList.add("far", "fa-copy");
     element.style.color = "";
 }, 1500);
 }
